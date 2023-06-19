b006669e39fc103a6f325b070c6016b499790c03
dpimenova <dpimenova@sigma-it.ru>
2023-06-19 14:05:55 +0300

    feat: add home and sidebar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TuiRootModule } from '@taiga-ui/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './components/home/home.module';
+import { SidebarModule } from './components/sidebar/sidebar.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [AppRoutingModule, BrowserAnimationsModule, BrowserModule, HomeModule, SidebarModule, TuiRootModule],
   providers: [],
   bootstrap: [AppComponent],
 })
