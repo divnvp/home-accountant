a00a8b79adc276c8d1bc81bced107651d9eef8a0
dpimenova <dpimenova@sigma-it.ru>
2023-06-22 21:35:15 +0300

    fix: dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { StateService } from './services/state.service';
+import { TuiBrightness } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  private onDark = this.service.darkThemeState$;
+
+  get nameTheme(): TuiBrightness | null {
+    let theme: 'onDark' | 'onLight' | null = null;
+    this.onDark.subscribe(value => (value ? (theme = 'onDark') : (theme = 'onLight')));
+
+    return theme;
+  }
+
+  constructor(private service: StateService) {}
+}
